ab2198e3b06fa2770dce846a76b69fd7ff52863c
Ernesto Gutierrez <ergutierrez39@gmail.com>
2021-09-20 23:42:44 -0600

    fix: Make the banner responsive

--- src/components/Characters/CharactersBanner.js
+++ src/components/Characters/CharactersBanner.js
@@ -5,8 +5,18 @@ function CharactersBanner(props) {
 
     return (
         <React.Fragment>
-            <Grid style={{ background: 'orange',paddingBottom: "12px" }} container columns={2} doubling textAlign='center' >
-                <Image src='/images/Marvel-Character-Banner.jpg' size='huge' />
+            <Grid 
+                style={{ background: 'orange', paddingBottom: "12px", borderBottom: "12px solid #cc8400" }} 
+                container 
+                columns={2} 
+                doubling 
+                textAlign='center'
+                className="cropped-banner" >
+                <Image 
+                    src='/images/Marvel-Character-Banner.jpg' 
+                    size='huge'
+                    className="full-banner"
+                />
             </Grid>
         </React.Fragment>
     );
